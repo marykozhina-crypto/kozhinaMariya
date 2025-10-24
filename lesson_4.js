6c4577879d0ca63a0b09625585c5a39480c3b84c
Mariya Kozhina <mary.kozhina@gmail.com>
2025-10-24 20:31:49 +0300

    init commit

---
+++ lesson_4.js
@@ -0,0 +1,50 @@
+// 1. Сумма чисел от 1 до N
+// Напиши функцию sumTo(n) , которая возвращает сумму всех чисел от 1 до n .
+
+function sumTo(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+alert( sumTo(20) );
+
+// 2. Степень числа
+// Напиши функцию pow(base, exponent) , которая возводит base в степень exponent с помощью цикла.
+
+function pow(base, exponent) {
+  let result = 1;
+  for (let i = 0; i < exponent; i++) {
+    result *= base;
+  }
+  return result;
+}
+alert( pow(3, 2) );
+
+// 3. Подсчёт чётных чисел
+// Функция countEven(n) должна вернуть количество чётных чисел от 1 до n .
+
+function countEven(n) {
+  let count = 0;
+  for (let i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+      count++; 
+    }
+  }
+    return count;
+}
+alert(countEven(10));
+
+// 4. Факториал числа
+// Реализуй factorial(n) с помощью цикла.
+
+function factorial(n) {
+  return (n != 1) ? n * factorial(n - 1) : 1;
+}
+
+alert( factorial(7) );
+
+// 5. Повтор строки
+// Напиши функцию repeatText(text, count) , которая возвращает строку, повторённую count раз подряд (без String.repeat ).
+
